6794bbb2e61d25b1f9178b60997ba6edaddc0517
SpringStudy <dnjsdyd78@gmail.com>
2024-10-08 19:37:45 +0900

    LV.1-2 User 엔티티 닉네임 칼럼추가, JwtUtil 닉네임 데이터추가, AuthUser 객체 닉네임 필드추가

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -35,7 +35,8 @@ class TodoControllerTest {
         // given
         long todoId = 1L;
         String title = "title";
-        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
+        String nickName = "nickName";
+        AuthUser authUser = new AuthUser(1L, "email", nickName, UserRole.USER);
         User user = User.fromAuthUser(authUser);
         UserResponse userResponse = new UserResponse(user.getId(), user.getEmail());
         TodoResponse response = new TodoResponse(
